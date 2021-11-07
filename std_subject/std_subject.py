1d7111f9f59287af83891501141e9dd42163e845
Administrator <1770996260@qq.com>
2021-11-07 14:28:17 +0800

    2021-11-07

--- std_subject/std_subject.py
+++ std_subject/std_subject.py
@@ -49,7 +49,50 @@ def get_subject_1992():
                 res.update({layer1_code + layer4_code:layer1_name + "·" + layer2_name + "·" + layer3_name + "·" + layer4_name})
     return res
 
-
+"""
+---------------------------------------------------------------------------------------
+"""
+def get_subject_2009():
+    res={}
+    the_former_code = ""
+    layer1_code = ""  # 一位
+    layer1_name = ""
+    layer2_code = ""  # 三位
+    layer2_name = ""  # 三位
+    layer3_code = ""  # 五位
+    layer3_name = ""
+    layer4_code = ""  # 七位
+    layer4_name = ""  # 七位
+    df = pd.read_excel("std_subject_2009.xlsx")
+    for i in range(len(df.values)):
+        item=df.values[i]
+        # print(item[0],item[1])
+        if (len(str(item[0])) == 1):
+            layer1_code = str(item[0])
+            layer1_name = item[1]
+            # print(layer1_code,layer1_name)
+        if (len(str(item[0])) == 3):
+            layer2_code = str(item[0])
+            layer2_name = item[1]
+            # print(layer2_code, layer2_name)
+        if (len(str(item[0])) == 5):
+            layer3_code = str(item[0])
+            layer3_name = item[1]
+            if(i!=(len(df.values)-1)):
+                if(len(str(df.values[i+1][0]))!=7):
+                    # print(layer1_code + layer3_code,layer1_name + "·" + layer2_name + "·" +layer3_name)
+                    res.update({layer1_code + layer3_code+"00":layer1_name + "·" + layer2_name + "·" +layer3_name})
+        if (len(str(item[0])) == 7):
+            layer4_code = str(item[0])
+            layer4_name = item[1]
+            # print(layer4_code, layer4_name)
+            if (layer4_code[:5] == layer3_code):
+                # print(layer1_code + layer4_code,layer1_name + "·" + layer2_name + "·" + layer3_name + "·" + layer4_name)
+                res.update({layer1_code + layer4_code:layer1_name + "·" + layer2_name + "·" + layer3_name + "·" + layer4_name})
+    return res
+"""
+---------------------------------------------------------------------------------------------------------------
+"""
 def get_conn():
     """
     :return: 连接，游标
@@ -73,12 +116,12 @@ def close_conn(conn, cursor):
 
 def into_mysql():
     global conn, cursor
-    res=get_subject_1992()
+    res=get_subject_2009()
     for k,v in res.items():
         print(k,v)
         try:
             conn,cursor=get_conn()
-            SQL="insert into std_subject_1992 (subject_code,subject_name) values ('"+k+"','"+v+"')"
+            SQL="insert into std_subject_2009 (year,subject_code,subject_name) values (2009,'"+k+"','"+v+"')"
             cursor.execute(SQL)
             conn.commit()
         except:
